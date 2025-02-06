57a1219652b8f9845959daad726c77480b1c5fb7
BhavyaAudisri <somisetty.bhavya@gmail.com>
2025-02-06 19:13:09 +0530

    modified

--- ec2/ec2.tf
+++ ec2/ec2.tf
@@ -1,6 +1,19 @@
 resource "aws_instance" "terraform" {
   ami                    = var.ami_id
   vpc_security_group_ids = [aws_security_group.allow-terraform.id]
+  instance_type          = var.instance_type
+  tags = {
+    name = "terraform-practice"
+  }
+}
+output "security" {
+ value = aws_security_group.allow-terraform.id
+}
+
+
+resource "aws_instance" "terraform1" {
+  ami                    = var.ami_id
+  vpc_security_group_ids = [aws_security_group.allow-terraform.id]
   #vpc_security_group_ids = output.security
   instance_type          = var.instance_type
   tags = {
